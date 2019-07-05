318b017ef337f44bbcb480ee7884d547ad035fc8
Lauri Kuusisto <laurimikael.kuusisto@gmail.com>
2019-07-05 22:23:55 +0300

    Bring bitwise operations

---
+++ BitShiftPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using BitShiftPractice.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitShiftPractice
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //The << operator shifts a number to the left by a specified number of bits. 
+            //Zeroes are added to the least significant bits.
+
+            BitwiseOperations.BitShiftToLeft(2, 4);
+
+
+            //perator shifts a number to the right by a specified number of bits. 
+            //The first operand is shifted to right by the number of bits specified by second operand.
+
+            //BitwiseOperations.BitShiftToRight(16, 4);
+
+
+            //Byte inversing turns 0 into 1 and 1 into 0. For example number 2 converted into 32-bits (basic integer has 32 bytes) binary is 00000000000000000000000000000010.
+            //That number inverted is 11111111111111111111111111111101 which in decimals is -3.
+
+            //BitwiseOperations.InverseBytes(2);
+
+
+            //If either of the bits on the same place is 1, the result is 1. Otherwise the result is 0.
+            //If value1 in binary 1001 (9 in decimal) and value2 is 1010 (10)
+            //after OR operation value is 1011 (11)
+
+            //BitwiseOperations.BitwiseOR(521, 23);
+
+
+            //If either of the bits is 0, the result is 0. Otherwise the result is 1.
+            //If value1 in binary 1001 (9 in decimal) and value2 is 1010 (10)
+            //after OR operation value is 1000 (8)
+
+            //BitwiseOperations.BitwiseAND(9, 10);
+
+
+            //If the corresponding bits are same, the result is 0. If the corresponding bits are different, the result is 1.
+            //If value1 in binary 1001 (9 in decimal) and value2 is 1010 (10)
+            //after XOR operation value is 0011 (3)
+
+            //BitwiseOperations.BitwiseXOR(9, 10);
+        }
+    }
+}
---
+++ BitShiftPractice/Properties/BitwiseOperations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitShiftPractice.Properties
+{
+    class BitwiseOperations
+    {
+        internal static void InverseBytes(short startValue)
+        {
+            Console.WriteLine($"***********************Bit inversing started***********************");
+            Console.WriteLine($"Start value is {startValue}");
+            Console.WriteLine($"Start value converted to bytes is {Convert.ToString(startValue, 2)}\n");
+
+            Console.WriteLine($"Bytes inversed is {Convert.ToString(~startValue, 2)}");
+            Console.WriteLine($"Which converted into decimals is {~startValue}\n");
+
+            Console.WriteLine($"\n***********************Bit inversing ended***********************\n\n\n");
+        }
+
+
+        internal static void BitShiftToRight(short startValue, int amount)
+        {
+            Console.WriteLine($"***********************Bit shift to right started***********************");
+            Console.WriteLine($"Start value is {startValue}");
+            Console.WriteLine($"Start value converted to bytes is {Convert.ToString(startValue, 2)}\n");
+
+            for (int i = 1; i < amount; i++)
+            {
+                Console.WriteLine($"Bytes shifted {i} times to the right is {Convert.ToString(startValue >> i, 2)}");
+                Console.WriteLine($"Which converted into decimals is {startValue >> i}\n");
+            }
+            Console.WriteLine($"\n***********************Bit shift to right ended***********************\n\n\n");
+        }
+
+        internal static void BitShiftToLeft(short startValue, int amount)
+        {
+            Console.WriteLine($"***********************Bit shift to left started***********************");
+            Console.WriteLine($"Start value is {startValue}");
+            Console.WriteLine($"Start value converted to bytes is {Convert.ToString(startValue, 2)}\n");
+
+            for (int i = 1; i < amount; i++)
+            {
+                Console.WriteLine($"Bytes shifted {i} times to the left is {Convert.ToString(startValue << i, 2)}");
+                Console.WriteLine($"Which converted into decimals is {startValue << i}\n");
+            }
+            Console.WriteLine($"\n***********************Bit shift to left ended***********************\n\n\n");
+        }
+
+        internal static void BitwiseOR(short value1, short value2)
+        {
+            Console.WriteLine($"***********************Bitwise OR operation started***********************");
+
+            Console.WriteLine($"Start values are {value1} and {value2}");
+            Console.WriteLine($"Value 1 converted to bytes is {Convert.ToString(value1, 2)}\n");
+            Console.WriteLine($"Value 2 converted to bytes is {Convert.ToString(value2, 2)}\n");
+
+            int OROperated = value1 | value2;
+            Console.WriteLine($"Bitwise OR operation between {Convert.ToString(value1, 2)} ({value1}) and {Convert.ToString(value2, 2)} ({value2}) is {Convert.ToString(OROperated, 2)} and in decimal {OROperated}");
+
+            Console.WriteLine($"\n***********************Bitwise OR operation ended***********************\n\n\n");
+        }
+
+        internal static void BitwiseAND(short value1, short value2)
+        {
+            Console.WriteLine($"***********************Bitwise And operation started***********************");
+
+            Console.WriteLine($"Start values are {value1} and {value2}");
+            Console.WriteLine($"Value 1 converted to bytes is {Convert.ToString(value1, 2)}\n");
+            Console.WriteLine($"Value 2 converted to bytes is {Convert.ToString(value2, 2)}\n");
+
+            int ANDOperated = value1 & value2;
+            Console.WriteLine($"Bitwise AND operation between {Convert.ToString(value1, 2)} ({value1}) and {Convert.ToString(value2, 2)} ({value2}) is {Convert.ToString(ANDOperated, 2)} and in decimal {ANDOperated}");
+
+            Console.WriteLine($"\n***********************Bitwise AND operation ended***********************\n\n\n");
+        }
+
+        internal static void BitwiseXOR(short value1, short value2)
+        {
+            Console.WriteLine($"***********************Bitwise XOR operation started***********************");
+
+            Console.WriteLine($"Start values are {value1} and {value2}");
+            Console.WriteLine($"Value 1 converted to bytes is {Convert.ToString(value1, 2)}\n");
+            Console.WriteLine($"Value 2 converted to bytes is {Convert.ToString(value2, 2)}\n");
+
+            int XOROperated = value1 ^ value2;
+            Console.WriteLine($"Bitwise AND operation between {Convert.ToString(value1, 2)} ({value1}) and {Convert.ToString(value2, 2)} ({value2}) is {Convert.ToString(XOROperated, 2)} and in decimal {XOROperated}");
+
+            Console.WriteLine($"\n***********************Bitwise XOR operation ended***********************\n\n\n");
+        }
+    }
+}
